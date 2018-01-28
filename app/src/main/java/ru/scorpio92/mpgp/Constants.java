31dfca6600f754e4211d9d821b59137d1c9da051
Scorpio92 <scorpio92@mail.ru>
2018-01-28 19:48:55 +1000

    Возможность отключать шифрование

--- app/src/main/java/ru/scorpio92/mpgp/Constants.java
+++ app/src/main/java/ru/scorpio92/mpgp/Constants.java
@@ -7,8 +7,10 @@ package ru.scorpio92.mpgp;
 public class Constants {
 
     public final static String PROJECT_URL = "https://github.com/mpgp";
-    public final static String AUTH_SERVER = "http://95.85.94.45";
+    public final static String AUTH_SERVER = "http://212.19.13.147";
     public final static int AUTH_SERVER_PORT = 8031;
     public final static String MAIN_SERVER = "18.218.84.243";
     public final static int MAIN_SERVER_PORT = 8181;
+
+    public final static boolean ENCRYPTION_ENABLED = false;
 }
